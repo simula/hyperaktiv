b3fa1ba65848101c8713460402d064b7e03a5e26
Steven Hicks <steven@simula.no>
2021-06-01 21:22:11 +0200

    update

--- experiments/predict_adhd.py
+++ experiments/predict_adhd.py
@@ -1,67 +1,61 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-import xgboost as xgb
-import lightgbm as lgb
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
 import numpy as np
 import pandas as pd
-import re
-from sklearn import preprocessing as pp
-from datetime import datetime
 
-from tsfresh.feature_selection.selection import select_features
+import re
+import argparse
 
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tsfresh.feature_selection.selection import select_features
 
-from utils import find_most_important_features
 from utils import k_fold_model_evaluation
-from utils import extract_features
 
-_RANDOM_SEED = 0
-_NUMBER_OF_FOLDS = 10
-_NUMBER_OF_FEATURES = 50
-_TEST_RATIO = .20
+parser = argparse.ArgumentParser(description="Script that runs the baseline experiments.")
 
-_PATH_TO_ACTIVITY_DATA = "C:\\Users\\Steven\\github\\hyperaktiv\\data\\activity_data"
-_PATH_TO_FEATURES = "C:\\Users\\Steven\\github\\hyperaktiv\\data\\features.csv"
-_PATH_TO_GT = "C:\\Users\\Steven\\github\\hyperaktiv\\data\\patient_data.csv"
-
-TS_FEATURE_PARAMETERS = {
-    "mean": None,
-    "count_below": [{"t": 0}],
-    "standard_deviation": None
-}
-
-_PARAMS_LORGREG = {
-    "penalty": "l2", "C": 1.0, "class_weight": "balanced",
-    "random_state": 0, "solver": "liblinear", "n_jobs": 1
-}
-
-_PARAMS_RFC = {
-    "n_estimators": 1000,
-    "max_features": "auto", "max_depth": None,
-    "min_samples_split": 2, "min_samples_leaf": 1,
-    "min_weight_fraction_leaf": 0.0,
-    "max_leaf_nodes": None, "bootstrap": True,
-    "oob_score": False, "n_jobs": -1, "random_state": 0,
-    "class_weight": "balanced"
-}
-
-_PARAMS_XGB = {
-    "n_estimators": 1000, "random_state": _RANDOM_SEED, "verbosity": 0,
-    'objective':'binary:logistic', "learning_rate": .0001
-}
-
-_PARAMS_LIGHTGB = {
-    "n_estimators": 1000, "random_state": _RANDOM_SEED, "verbosity": 0,
-    "objective": "binary", "learning_rate": .0001
-}
+parser.add_argument("-x", "--x-file-path", type=str, required=True)
+parser.add_argument("-y", "--y-file-path", type=str, required=True)
+parser.add_argument("-o", "--output-file-path", type=str, default="results.txt")
+parser.add_argument("-k", "--k-folds", type=int, default=10)
+parser.add_argument("-t", "--test-ratio", type=float, default=0.20)
+parser.add_argument("-s", "--random-seed", type=int, default=0)
 
 if __name__ == "__main__":
+    
+    args = parser.parse_args()
 
-    # dataX = extract_features(_PATH_TO_ACTIVITY_DATA, TS_FEATURE_PARAMETERS).sort_values(by="ID")
-    dataX = pd.read_csv(_PATH_TO_FEATURES, sep=";").sort_values(by="ID")
-    dataY = pd.read_csv(_PATH_TO_GT, sep=";").sort_values(by="ID")
+    _PARAMS_LORGREG = {
+        "penalty": "l2", "C": 1.0, "class_weight": "balanced",
+        "random_state": 0, "solver": "liblinear", "n_jobs": 1
+    }
+
+    _PARAMS_RFC = {
+        "n_estimators": 1000,
+        "max_features": "auto", "max_depth": None,
+        "min_samples_split": 2, "min_samples_leaf": 1,
+        "min_weight_fraction_leaf": 0.0,
+        "max_leaf_nodes": None, "bootstrap": True,
+        "oob_score": False, "n_jobs": -1, "random_state": 0,
+        "class_weight": "balanced"
+    }
+
+    _PARAMS_XGB = {
+        "random_state": args.random_seed, "verbosity": 0,
+        'objective':'binary:logistic'
+    }
+
+    _PARAMS_LIGHTGB = {
+        "random_state": args.random_seed, "verbosity": 0,
+        "objective": "binary"
+    }
+
+    dataX = pd.read_csv(args.x_file_path, sep=";").sort_values(by="ID")
+    dataY = pd.read_csv(args.y_file_path, sep=";").sort_values(by="ID")
 
     dataX = dataX.fillna(0)
 
@@ -74,9 +68,6 @@ if __name__ == "__main__":
     # Match X and Y data
     dataY = dataY[dataY["ID"].isin(dataX["ID"])]
     dataX = dataX[dataX["ID"].isin(dataY["ID"])]
-    
-    dataX = dataX.sort_values(by="ID")
-    dataY = dataY.sort_values(by="ID")
 
     dataY = dataY.set_index("ID")
     dataX = dataX.set_index("ID")
@@ -85,51 +76,43 @@ if __name__ == "__main__":
     dataY = dataY["ADHD"].copy()
 
     # Find relevant features using tsfresh
-    # dataX = select_features(dataX, dataY)
+    dataX = select_features(dataX, dataY)
     
-    scaler = pp.StandardScaler(copy=True)
+    scaler = StandardScaler(copy=True)
     dataX.loc[:, dataX.columns] = scaler.fit_transform(dataX[dataX.columns])
     
     X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(
         dataX,
         dataY,
-        test_size=_TEST_RATIO,
-        random_state=_RANDOM_SEED,
+        test_size=args.test_ratio,
+        random_state=args.random_seed,
         stratify=dataY)
-
-    # Find most relevant features using linear regression
-    if _NUMBER_OF_FEATURES is not None:
-
-        important_features = find_most_important_features(X_TRAIN, Y_TRAIN, _NUMBER_OF_FEATURES)
-
-        X_TRAIN = X_TRAIN[ important_features ]
-        X_TEST = X_TEST[ important_features ]
         
     metric_names = ["ACC", "PREC", "REC", "F1", "MCC"]
 
     stratified_train_eval, stratified_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "stratified", "random_state": 0 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     most_frequent_train_eval, most_frequent_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "most_frequent", "random_state": 0 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     prior_train_eval, prior_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "prior", "random_state": 0 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     minor_train_eval, minor_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "constant", "random_state": 0, "constant": 1 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     major_train_eval, major_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "constant", "random_state": 0, "constant": 0 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     random_train_eval, random_test_eval = k_fold_model_evaluation(DummyClassifier, { "strategy": "uniform", "random_state": 0 },
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
 
     logreg_train_eval, logreg_test_eval = k_fold_model_evaluation(LogisticRegression, _PARAMS_LORGREG,
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
     rfc_train_eval, rfc_test_eval = k_fold_model_evaluation(RandomForestClassifier, _PARAMS_RFC,
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
-    xgb_train_eval, xgb_test_eval = k_fold_model_evaluation(xgb.XGBClassifier, _PARAMS_XGB,
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
-    gbm_train_eval, gbm_test_eval = k_fold_model_evaluation(lgb.LGBMClassifier, _PARAMS_LIGHTGB,
-        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=_NUMBER_OF_FOLDS, random_state=_RANDOM_SEED)
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
+    xgb_train_eval, xgb_test_eval = k_fold_model_evaluation(XGBClassifier, _PARAMS_XGB,
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
+    gbm_train_eval, gbm_test_eval = k_fold_model_evaluation(LGBMClassifier, _PARAMS_LIGHTGB,
+        X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, n_splits=args.k_folds, random_state=args.random_seed)
 
-    with open("adhd_prediction.txt", "w") as f:
+    with open(args.output_file_path, "w") as f:
         f.write("***CROSS-VALIDATION PERFORMANCE***\n")
         f.write("MODEL\t" + "\t ".join(metric_names) + "\n")
         f.write("Rand\t" + "\t ".join([ "%.2f" % (np.mean(random_train_eval[name])) for name in metric_names ]) + "\n")
--- experiments/utils.py
+++ experiments/utils.py
@@ -1,19 +1,10 @@
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-import pandas as pd
+from collections import defaultdict
 from copy import deepcopy
 
-from collections import defaultdict
-from datetime import datetime
-
-import csv
-import glob
-import os
+import numpy as np
 
 from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
-
-from tsfresh import extract_features as ts_extract_features
 
 def calculate_classification_metrics(y_preds, y_trues):
     y_preds = np.round(y_preds)
@@ -25,45 +16,6 @@ def calculate_classification_metrics(y_preds, y_trues):
         "F1": metrics.f1_score(y_trues, y_preds, average="binary"),
         "MCC": metrics.matthews_corrcoef(y_trues, y_preds)
     }
-
-def extract_features(data_dir, fc_parameters, day_index=None):
-
-    def read_activity_file(filepath, patient_id):
-        data = [ ]
-        with open(filepath) as f:
-            csv_reader = csv.reader(f, delimiter=";")
-            next(csv_reader)
-            for line in csv_reader:
-                data.append([ datetime.strptime(line[0], "%m-%d-%Y %H:%M"), int(line[1].split(" ")[0])])
-        data = pd.DataFrame(data, columns=["TIME", "ACC"])
-        data["ID"] = patient_id
-        data["TIME"] = pd.to_datetime(data['TIME'], format="%m-%d-%Y %H:%M")
-        if day_index is not None:
-            gb = data.groupby(pd.Grouper(key="TIME", freq='D'))
-            day_data = gb.get_group((list(gb.groups)[day_index]))
-            return day_data
-        return data
-
-    patient_records = []
-
-    for filepath in glob.glob(os.path.join(data_dir, "*.csv")):
-        patient_id = int(os.path.splitext(os.path.basename(filepath))[0].split("_")[-1])
-        data = read_activity_file(filepath, patient_id)
-        patient_records.append(data)
-
-    patient_records = pd.concat(patient_records)
-
-    dataX = ts_extract_features(patient_records, default_fc_parameters=fc_parameters,
-        column_id="ID", column_value="ACC", column_sort="TIME", n_jobs=0, show_warnings=False)
-    dataX['ID'] = dataX.index
-    return dataX
-
-def find_most_important_features(x_data, y_data, n_features):
-    model = LogisticRegression()
-    model.fit(x_data, y_data)
-    most_important_features = pd.DataFrame(data={ 'Attribute': x_data.columns, 'Importance': model.coef_[0] })
-    most_important_features = most_important_features.sort_values(by='Importance', ascending=False)[:n_features]
-    return list(most_important_features.Attribute.values)
 
 def k_fold_model_evaluation( model_func, model_parameters, x_train, y_train, x_test, y_test,
      fit_parameters={}, n_splits=10, shuffle=True, random_state=0 ):
